3fc57062b668fc9d1079980909a9a10d2f1516b8
zxcvpn011 <zxcvpn011@gmail.com>
2021-04-10 15:07:25 +0200

    edit span elements script and added one line style for span

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -15,30 +15,40 @@ if ($('.smart-scroll').length > 0) { // check if element exists
         last_scroll_top = scroll_top;
     });
 }
-
 const last = document.getElementById('last'); // define parent contain all cards
 last.addEventListener("click", (event)=>{ // add click event in parent
     if(event.target.tagName === 'I'){ // Write 'I' not 'i'tag name must be upper case
+        //console.log(event.target.parentNode)
         const icon = event.target; // define the target element
         const card = icon.parentNode;
         if(icon.getAttribute('value') === 'remove'){ // get attribute from i element to remove target card
             last.removeChild(card); // remove target card
         }else if(icon.getAttribute('value') === 'edit'){ // get attribute from i element to edit target card
-            const span = card.firstElementChild;
-            const input = document.createElement('input'); // create input text to carry span value and can edit it
-            input.type = 'text'; // add type to input
-            input.value = span.textContent; // take the text from span to the input
-            card.insertBefore(input, span);// change the place between input and span
-            card.removeChild(span); // remove span and add input in the same place
+            const span = card.getElementsByTagName('span');
+            for(let i = 0; i < span.length; i++) {
+                const input = document.createElement('input'); // create input text to carry span value and can edit it
+                input.type = 'text'; // add type to input
+                input.value = span[i].textContent;  // take the text from span to the input
+                card.insertBefore(input, span[0])
+                span[i].remove() // remove span and add input in the same place
+                i--; //this decreament because when remove a span element the span index getting decrease 
+                //for example if the span length is 4 when we remove one they will be 3 so you should decrease that's why the index should to decrease
+            }
             icon.className = 'fa fa-check-circle green-color'; // change icon from 'fa-edit' to 'fa-check-circle' to change icon shape
-            // icon.setAttribute('id',''); // change ID from 'edit' to 'green-color' to make icon have green color when hover on it
-            icon.setAttribute('value',''); // remove value from icon to enter in the next 'if else'
+            icon.setAttribute('id',''); // change ID from 'edit' to 'green-color' to make icon have green color when hover on it
+            icon.setAttribute('value',''); // remove value from icon to enter in the next 'if else
         }else if(icon.getAttribute('value') === ''){
-            const input = card.firstElementChild;
-            const span = document.createElement('span'); // create span to carry the input value
-            span.textContent = input.value; // take the text from input to the span
-            card.insertBefore(span, input); // change the place between input and span
-            card.removeChild(input); // remove span and add input in the same place
+            const input = card.getElementsByTagName('input');
+            let class_arr = ["name", "date", "time", "duration"] //span class array
+            for(i = 0; i < input.length; i++) {
+                const span = document.createElement('span'); // create span to carry the input value
+                span.textContent = input[i].value; // take the text from input to the span
+                span.setAttribute("class", "subject subject-"+class_arr[i]) //back the class to span elements
+                class_arr.shift() //remove first index of array elements every time
+                card.insertBefore(span, input[0]); // change the place between input and span
+                input[i].remove(); // remove span and add input in the same place
+                i--;
+            }    
             icon.className = 'fa fa-edit'; // change icon from 'fa-check-circle' to 'fa fa-edit' to change icon shape
             // icon.setAttribute('id',''); // remove ID 'green-color' to make icon have blue color when hover on it
             icon.setAttribute('class','fa fa-edit edit'); // change ID from 'green-color' to 'edit' to make icon have green color when hover on it
